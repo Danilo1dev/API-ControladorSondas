f564cb78b7efb34e38486e44807b4d69609d4150
Danilo1dev <danilofoxlog@gmail.com>
2022-09-21 00:13:04 -0300

    ajuste na anotacao em SondaRequest

--- src/main/java/br/com/elo7/contraladorsondas/sonda/application/api/SondaRequest.java
+++ src/main/java/br/com/elo7/contraladorsondas/sonda/application/api/SondaRequest.java
@@ -1,9 +1,16 @@
 package br.com.elo7.contraladorsondas.sonda.application.api;
 
 import javax.validation.constraints.NotBlank;
-import lombok.Value;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Value
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@ToString
+@EqualsAndHashCode
 public class SondaRequest {
 	 @NotBlank
 	 private String nome;
